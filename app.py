bdb865846bc7e4aa3730ce6be474c71611f7171b
123456789260 <134251322+123456789260@users.noreply.github.com>
2023-05-24 09:26:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def predict_img(img):
 html_temp = """
     <div style="background-color:#2F3C7E;padding:10px;margin-bottom: 25px">
     <h2 style="color:white;text-align:center;">COVID Detection</h2>
-    <p style="color:white;text-align:center;" >This is a <b>Streamlit</b> app use for prediction of the <b> COVID</b>.</p>
+    <p style="color:white;text-align:center;" >This is a <b>Streamlit</b> app use for prediction of the <b> COVID-19</b>.</p>
     </div>
     """
 st.markdown(html_temp,unsafe_allow_html=True)
